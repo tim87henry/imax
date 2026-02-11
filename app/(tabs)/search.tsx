7fbfba45f677ee25726db7f2fe63dcd1849db921
Timothy <tim87henry@gmail.com>
2026-02-11 22:09:02 +1100

    Added connectivity and logic to create and update Appwrite DB.

--- app/(tabs)/search.tsx
+++ app/(tabs)/search.tsx
@@ -1,6 +1,7 @@
 import { icons } from '@/constants/icons'
 import { images } from '@/constants/images'
 import { fetchMovies } from '@/services/api'
+import { updateSearchCount } from '@/services/appwrite'
 import useFetch from '@/services/useFetch'
 import React, { useEffect, useState } from 'react'
 import { ActivityIndicator, FlatList, Image, Text, View } from 'react-native'
@@ -25,6 +26,9 @@ const Search = () => {
     const timeoutId = setTimeout(async () => {
       if (searchQuery.trim()) {
         await loadMovies(); 
+        if (movies && movies.length > 0 && searchQuery.trim()) {
+          await updateSearchCount(searchQuery, movies[0])
+        }
       } else {
         reset()
       }
--- services/appwrite.ts
+++ services/appwrite.ts
@@ -1,3 +1,58 @@
+import { Client, Databases, ID, Query } from "react-native-appwrite";
+
+const DATABASE_ID = process.env.EXPO_PUBLIC_APPWRITE_DATABASE_ID!;
+const COLLECTION_ID = process.env.EXPO_PUBLIC_APPWRITE_TABLE_ID!;
+const PROJECT_ID = process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID!;
+
+const client = new Client()
+    .setEndpoint('https://syd.cloud.appwrite.io/v1')
+    .setProject(PROJECT_ID);
+
+const database = new Databases(client);
+
 export const updateSearchCount = async (query: string, movie: Movie) => {
-    
+    console.log("Hello ")
+    console.log(movie.title)
+    console.log(query)
+    try {
+        const result = await database.listDocuments(DATABASE_ID, COLLECTION_ID, [
+            Query.equal('searchTerm', query)
+        ])
+
+        console.log(result)
+
+        if (result.documents.length > 0) {
+            console.log("Updating count")
+            console.log(result.documents[0].count)
+            await database.updateDocument(DATABASE_ID, COLLECTION_ID, result.documents[0].$id, {
+                count: result.documents[0].count + 1
+        })
+        } else {
+            await database.createDocument(DATABASE_ID, COLLECTION_ID, ID.unique(), {
+                searchTerm: query,
+                movie_id: movie.id,
+                title: movie.title,
+                count: 1,
+                poster_url: `https://image.tmdb.org/t/p/w500${movie.poster_path}`,
+        })
+    }
+    } catch (error) {
+        console.log("Error accessing search count", error);
+        throw error;
+    }
 }
+
+// export const getTrendingMovies = async(): Promise<TrendingMovie[] | undefined> => {
+//     try {
+//         const result = await database.listDocuments(DATABASE_ID, COLLECTION_ID, [
+//             Query.limit(5),
+//             Query.orderDesc('count')
+//         ])
+//         console.log("Trending")
+//         console.log(result.documents)
+
+//     } catch(error) {
+//         console.log(error)
+//         throw undefined;
+//     }
+// }
